b9ac03e3701429f3f54b80458de66389bad22ad5
Monyuy Divine <dbootable@gmail.com>
2023-04-20 10:48:29 +0300

    authSlice setup Complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 //import Router from "./routes";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -22,49 +22,53 @@ import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar/calendar";
 import SignIn from "./scenes/Authentication/login/login";
 import SignUp from "./scenes/Authentication/Registration/register";
+import { useSelector } from "react-redux";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
-  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  //const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  const isLoggedIn = useSelector((state) => state.isLoggedIn.value);
+  console.log(isLoggedIn + "ok");
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {isLoggedIn ? (
-        <>
-        <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-           <Routes>
-             <Route path="/" element={<Dashboard />} />
-             <Route path="/team" element={<Team />} />
-             <Route path="/contacts" element={<Contacts />} />
-             <Route path="/invoices" element={<Invoices />} />
-             <Route path="/matter" element={<Matter />} />
-             <Route path="/tasks" element={<Tasks />} />
-             <Route path="/activities" element={<Activities />} />
-             <Route path="/files" element={<Files />} />
-             <Route path="/form" element={<Form />} />
-             <Route path="/bar" element={<Bar />} />
-             <Route path="/pie" element={<Pie />} />
-             <Route path="/line" element={<Line />} />
-             <Route path="/faq" element={<FAQ />} />
-             <Route path="/calendar" element={<Calendar />} />
-             <Route path="/geography" element={<Geography />} />
-           </Routes>
-         </main>
-        </div>
-        </>) :(
-            <Routes>
-             <Route path='/' element={<SignIn />} />
-             <Route path='/signup' element={<SignUp />} />
-           </Routes>
-         )}
+          <>
+            <div className="app">
+              <Sidebar isSidebar={isSidebar} />
+              <main className="content">
+                <Topbar setIsSidebar={setIsSidebar} />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/team" element={<Team />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/invoices" element={<Invoices />} />
+                  <Route path="/matter" element={<Matter />} />
+                  <Route path="/tasks" element={<Tasks />} />
+                  <Route path="/activities" element={<Activities />} />
+                  <Route path="/files" element={<Files />} />
+                  <Route path="/form" element={<Form />} />
+                  <Route path="/bar" element={<Bar />} />
+                  <Route path="/pie" element={<Pie />} />
+                  <Route path="/line" element={<Line />} />
+                  <Route path="/faq" element={<FAQ />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/geography" element={<Geography />} />
+                </Routes>
+              </main>
+            </div>
+          </>
+        ) : (
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        )}
       </ThemeProvider>
-     </ColorModeContext.Provider>
+    </ColorModeContext.Provider>
   );
 }
 
