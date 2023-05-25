39ac8654f4ae96a9747023a60e3ba0a1a575ec6d
Monyuy Divine <dbootable@gmail.com>
2023-05-25 19:25:11 +0300

    case tabs handleChange function

--- src/scenes/matter/index.jsx
+++ src/scenes/matter/index.jsx
@@ -3,12 +3,12 @@ import { Box, Tab, Typography } from "@mui/material";
 import { tokens } from "../../theme";
 import { useTheme } from "@mui/material";
 
-const Case = ()=>{
-       
-   return(
-       <Box>
-       </Box>
-   
-   );
-}
+const Case = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+  return <Box></Box>;
+};
 export default Case;
