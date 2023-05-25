31a5a3c11ca8c93be9c51921ceeb017a0c074cac
Monyuy Divine <dbootable@gmail.com>
2023-05-25 19:27:31 +0300

    tabs index 100%

--- src/scenes/matter/index.jsx
+++ src/scenes/matter/index.jsx
@@ -9,6 +9,33 @@ const Case = () => {
   const handleTabChange = (event, newTabIndex) => {
     setTabIndex(newTabIndex);
   };
-  return <Box></Box>;
+  return (
+    <Box>
+      <Box>
+        <Tabs value={tabIndex} onChange={handleTabChange}>
+          <Tab label="Category" />
+          <Tab label="Court Case" />
+          <Tab label="Settlement" />
+        </Tabs>
+      </Box>
+      <Box sx={{ padding: 2 }}>
+        {tabIndex === 0 && (
+          <Box>
+            <Typography>The first tab</Typography>
+          </Box>
+        )}
+        {tabIndex === 1 && (
+          <Box>
+            <Typography>The second tab</Typography>
+          </Box>
+        )}
+        {tabIndex === 2 && (
+          <Box>
+            <Typography>The third tab</Typography>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
 };
 export default Case;
