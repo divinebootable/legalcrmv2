822bf2c472497c88cd61df34a55848b805326855
Monyuy Divine <dbootable@gmail.com>
2023-06-12 23:50:39 +0300

    serverConfig

---
+++ src/serverConfig.js
@@ -0,0 +1,9 @@
+const URL = ``;
+
+export default {
+  //Client api service
+  All_Clients: URL + `AllClients`,
+  ADD_Client: URL + `AddClient`,
+  UPDATE_Client: URL + `UpdateClient`,
+  DELETE_Client: URL + `DeleteClient`,
+};
