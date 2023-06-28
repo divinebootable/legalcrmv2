75e271d0e4558e88b7877a5123f4bd552e8d2252
Monyuy Divine <dbootable@gmail.com>
2023-06-28 18:31:22 +0300

    authSlice register  createAsyncThunk

--- src/features/authentication/authSlice.js
+++ src/features/authentication/authSlice.js
@@ -1,5 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { setMessage } from "./messageSlice";
 import AuthService from "../../services/auth.service";
 
 const user = JSON.parse(localStorage.getItem("user"));
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({}, thunkAPI) => {
+    try {
+      const response = await AuthService.register();
+      thunkAPI.dispatch(setMessage(response.data.message));
+      return response.data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
