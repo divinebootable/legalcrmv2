35e4c647fe250686e68342c906dc21c4b2b66bf2
Monyuy Divine <dbootable@gmail.com>
2023-06-30 21:41:59 +0300

    register law firm field

--- src/scenes/Authentication/Registration/register.jsx
+++ src/scenes/Authentication/Registration/register.jsx
@@ -112,79 +112,113 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="phone"
-                  label="Phone number"
-                  type="tel"
-                  id="phonenumber"
-                  autoComplete="phone number"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+          <Box sx={{ mt: 3 }}>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
             >
-              Get Started
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
+              {({ errors, touched }) => (
+                <Form>
+                  {!successful && (
+                    <div>
+                      <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                          <TextField
+                            className={
+                              "form-control" +
+                              (errors.account_name && touched.account_name
+                                ? "is-invalid"
+                                : "")
+                            }
+                            autoComplete="given-name"
+                            name="account_name"
+                            required
+                            fullWidth
+                            id="AccountName"
+                            label="Account Name"
+                            autoFocus
+                          />
+                          <ErrorMessage
+                            name="account_name"
+                            component="grid"
+                            className="invalid-feedback"
+                          />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                          <TextField
+                            className={
+                              "form-control" +
+                              (errors.law_firm && touched.law_firm
+                                ? "is-invalid"
+                                : "")
+                            }
+                            required
+                            fullWidth
+                            id="lawfirm"
+                            label="Law Firm"
+                            name="law_firm"
+                            autoComplete="law firm"
+                          />
+                          <ErrorMessage
+                            name="law_firm"
+                            component="grid"
+                            className="invalid-feedback"
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            required
+                            fullWidth
+                            name="phone"
+                            label="Phone number"
+                            type="tel"
+                            id="phonenumber"
+                            autoComplete="phone number"
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <FormControlLabel
+                            control={
+                              <Checkbox
+                                value="allowExtraEmails"
+                                color="primary"
+                              />
+                            }
+                            label="I want to receive inspiration, marketing promotions and updates via email."
+                          />
+                        </Grid>
+                      </Grid>
+                      <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                      >
+                        Get Started
+                      </Button>
+                      <Grid container justifyContent="flex-end">
+                        <Grid item>
+                          <Link href="/" variant="body2">
+                            Already have an account? Sign in
+                          </Link>
+                        </Grid>
+                      </Grid>
+                    </div>
+                  )}
+                </Form>
+              )}
+            </Formik>
           </Box>
         </Box>
         <Copyright sx={{ mt: 5 }} />
